121da7182be2f5058992f2ab44f72addfb005f79
869100035 <869100035@qq.com>
2022-05-06 14:02:38 +0800

    提交点名

--- src/main/java/com/markerhub/controller/TestController.java
+++ src/main/java/com/markerhub/controller/TestController.java
@@ -3,11 +3,13 @@ package com.markerhub.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @RestController
 public class TestController {
 
     @GetMapping("/tt")
     public String test(){
-        return "hello linux jenkins noMysql";
+        return  "ok";
     }
 }
